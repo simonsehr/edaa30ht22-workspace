579e8193b5aff5a03b874d68977d8086db0152f3
Simon Sehr <beenjamin.simon@gmail.com>
2022-10-01 11:21:44 +0200

    v1

---
+++ lab2/src/textproc/GeneralWordCounter.java
@@ -0,0 +1,15 @@
+package textproc;
+
+public class GeneralWordCounter implements TextProcessor{
+
+    public void process(String w){
+
+    }
+
+    public void report(){
+        
+    }
+
+
+    
+}
---
+++ lab2/src/textproc/Holgersson.java
@@ -0,0 +1,43 @@
+package textproc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Holgersson {
+
+	public static final String[] REGIONS = { "blekinge", "bohuslän", "dalarna", "dalsland", "gotland", "gästrikland",
+			"halland", "hälsingland", "härjedalen", "jämtland", "lappland", "medelpad", "närke", "skåne", "småland",
+			"södermanland", "uppland", "värmland", "västerbotten", "västergötland", "västmanland", "ångermanland",
+			"öland", "östergötland" };
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+
+		ArrayList<TextProcessor> list = new ArrayList<TextProcessor>();
+		list.add(new SingleWordCounter("nils"));
+		list.add(new SingleWordCounter("norge"));
+		list.add(new MultiWordCounter(REGIONS));
+
+		Scanner s = new Scanner(new File("./lab2/nilsholg.txt"));
+		s.findWithinHorizon("\uFEFF", 1);
+		s.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+"); // se handledning
+
+		while (s.hasNext()) {
+			String word = s.next().toLowerCase();
+
+			for(TextProcessor a : list){
+				a.process(word);
+			}
+
+		}
+
+		s.close();
+
+		for(TextProcessor a : list){
+			a.report();
+		}
+
+	}
+}
