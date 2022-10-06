a4271c7fe9b2b87b226fc8d41933c67cb0f1b8cd
Simon Sehr <beenjamin.simon@gmail.com>
2022-10-06 13:49:58 +0200

    last

--- lab3/src/textproc/BookReaderController.java
+++ lab3/src/textproc/BookReaderController.java
@@ -2,10 +2,14 @@ package textproc;
 import java.awt.*;
 import javax.swing.*;
 import java.util.Map.Entry;
+import java.awt.event.*;  
 
 
 
 public class BookReaderController {
+
+    JRadioButton b1, b2;
+    JButton b;
 
 
     public BookReaderController(GeneralWordCounter counter) {
@@ -25,8 +29,6 @@ public class BookReaderController {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container pane = frame.getContentPane();
 
-
-
         JScrollPane scrollPane = new JScrollPane(myList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
         JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setPreferredSize(new Dimension(width, height));
@@ -34,35 +36,60 @@ public class BookReaderController {
         //JRadioButton sortAlphabetical = new JRadioButton("Alphabetically sorted");
         //JRadioButton sortFrequency = new JRadioButton("Frequency sorted");
 
+
+        //Knappar
+        b1 = new JRadioButton("Alphabetic");
+        b2 = new JRadioButton("Frequency");
+        ButtonGroup group = new ButtonGroup();
+        group.add(b1);
+        group.add(b2);
+
+        b1.addActionListener(e -> listModel.sort(
+            (x, y) -> ((Entry<String, Integer>) x).getKey().compareTo(((Entry<String, Integer>) y).getKey())));
+        b2.addActionListener(e -> listModel
+            .sort((x, y) -> -(((Entry<String, Integer>) x).getValue() - ((Entry<String, Integer>) y).getValue()))); 
+
+
         JPanel panel = new JPanel();
-        JButton Alphabetical = new JButton("Alphabetic");
-        JButton Frequency = new JButton("Frequency");
+        JButton Exit = new JButton("Exit");
 
-        Alphabetical.setSelected(true);
-        
-        panel.add(Alphabetical);
-        panel.add(Frequency);
+        //Lägger till knappar i panelen
+        panel.add(b1);
+        panel.add(b2);
 
+        b1.addActionListener( (n) -> { System.out.println("Alphabetical"); } );
+        b2.addActionListener( (n) -> { System.out.println("Frequency"); } );
 
+        //Sökfält och sökfältsknapp
+        JTextField searchField = new JTextField();
+        JButton searchButton = new JButton("Search");
+        panel.add(searchField);
+        panel.add(searchButton);
 
-
-
-
-
-
-        
 
 
         pane.add(scrollPane, BorderLayout.NORTH);
         pane.add(panel, BorderLayout.CENTER);
+
+        //program exit-knapp
+        panel.add(Exit);
+
+        //Stänger systemet vid tryck av exitknapp
+        Exit.addActionListener((n) -> { System.exit(0); });
  
 
-        //frame.setSize(1000, 1000);
         frame.pack();
         frame.setVisible(true);
 
-
-
-
     }
+
+    public void actionPerformed(ActionEvent e){    
+        if(b1.isSelected()){    
+        System.out.println("Alphabetic");        
+        }    
+        if(b2.isSelected()){    
+        System.out.println("Frequency");    
+        }    
+        }  
+
 }
